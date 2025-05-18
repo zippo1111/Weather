c2e6a6ec9fcc56dadf016c35a1d97934f8b4cb8f
Mangust <Mangust>
2025-05-18 20:15:55 +0300

    Initial Commit

---
+++ WeatherApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApi
+//
+//  Created by Mangust on 18.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
